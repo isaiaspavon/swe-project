f8974febd16a337085071a413523b5ec860dc95d
Isaias Pavon <isaiaspavon22@gmail.com>
2025-06-29 16:06:06 -0400

    React+vite initialized. Firebase auth/db configured into frontend. New structure created for project.

---
+++ README.md
@@ -0,0 +1,19 @@
+## Instructions
+
+To initialize on your individual IDE insert the following commands in your terminal:
+
+```
+$ git clone https://github.com/isaiaspavon/swe-project.git
+```
+
+```
+$ git init
+```
+
+```
+$ git remote add origin git@github.com:isaiaspavon/swe-project.git
+```
+
+```
+$ npm install
+```
---
+++ frontend/src/firebaseConfig.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+
+// Firebase config file
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAjUpA0011FObYX3xsKF8G9zWxUaRlk9ig",
+  authDomain: "swe-database-3ba25.firebaseapp.com",
+  databaseURL: "https://swe-database-3ba25-default-rtdb.firebaseio.com",
+  projectId: "swe-database-3ba25",
+  storageBucket: "swe-database-3ba25.firebasestorage.app",
+  messagingSenderId: "504638588087",
+  appId: "1:504638588087:web:531b9a9191ac1da8ee7932",
+  measurementId: "G-3DRB2W92KY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//Export Auth and Realtime DB services
+export const auth = getAuth(app);
+export const db = getDatabase(app);
