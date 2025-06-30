5c8198a1015507dcde876edc98fe13464ce264de
Isaias Pavon <isaiaspavon22@gmail.com>
2025-06-29 21:55:49 -0400

    Updated DB functionality to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   const [count, setCount] = useState(0)
 
---
+++ frontend/src/components/BookCard.jsx
@@ -0,0 +1,18 @@
+// BookCard.jsx
+// This file contains a reusable component to display book details in a card layout
+
+import React from "react";
+
+const BookCard = ({ book, onAddToCart }) => {
+  return (
+    <div className="book-card">
+      <img src={book.image} alt={book.title} className="book-image" />
+      <h3 className="book-title">{book.title}</h3>
+      <p className="book-author">by {book.author}</p>
+      <p className="book-price">${book.price.toFixed(2)}</p>
+      <button onClick={() => onAddToCart(book)}>Add to Cart</button>    {/* This prop doesn't have functionality yet*/}
+    </div>
+  );
+};
+
+export default BookCard;
